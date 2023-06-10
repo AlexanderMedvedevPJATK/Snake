2652ead4757b78f3a93197301068ea196e0a88cd
Alexander Medvedev <s28572@pjwstk.edu.pl>
2023-06-10 18:27:28 +0200

    buffer introduced

--- Snake.java
+++ Snake.java
@@ -1,14 +1,16 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Snake extends Thread implements KeyListener {
-    private int direction;
+//    private int direction;
     private int length;
     private final List<int[]> piecesList = new ArrayList<>();
     private final int[] movementBuffer;
     private int bufferContains;
+    boolean bufferUtilized;
     private final SnakeMoveListener field;
 
     public Snake(SnakeMoveListener field) {
@@ -20,9 +22,12 @@ public class Snake extends Thread implements KeyListener {
         piecesList.add(new int[] {0, 0});
         piecesList.add(new int[] {0, 0});
 
-        this.direction = 1; // 0 - UP, 1 - RIGHT, 2 - DOWN, 3 - LEFT
+        // this.direction = 1; // 0 - UP, 1 - RIGHT, 2 - DOWN, 3 - LEFT
+        this.bufferContains = 1;
+        this.bufferUtilized = true;
+        this.movementBuffer = new int[] {1, -1};
+
         this.length = 1;
-        this.movementBuffer = new int[2];
     }
 
 
@@ -36,12 +41,12 @@ public class Snake extends Thread implements KeyListener {
             try {
                 if(foodFound) {
                     field.spawnFood();
-                    foodFound = field.moveHead(new MoveEvent(this, direction, piecesList));
+                    foodFound = field.moveHead(new MoveEvent(this, movementBuffer[0], piecesList));
                     length++;
                     System.out.println(length);
                     foodEaten = true;
                 } else {
-                    foodFound = field.moveSnake(new MoveEvent(this, direction, piecesList));
+                    foodFound = field.moveSnake(new MoveEvent(this, movementBuffer[0], piecesList));
                 }
             } catch (Exception e) {
                 System.out.println("Game over!");
@@ -68,7 +73,7 @@ public class Snake extends Thread implements KeyListener {
             }
             if(length > 3 && foodEaten) piecesList.add(1,
                     new int[] {piecesList.get(0)[0], piecesList.get(0)[1]});
-            switch (direction) { // change head position
+            switch (movementBuffer[0]) { // change head position
                 case 0 -> piecesList.get(0)[0]--;
                 case 1 -> piecesList.get(0)[1]++;
                 case 2 -> piecesList.get(0)[0]++;
@@ -85,6 +90,8 @@ public class Snake extends Thread implements KeyListener {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+            if(bufferUtilized) removeDirection();
+            bufferUtilized = true;
         }
     }
     @Override
@@ -95,13 +102,13 @@ public class Snake extends Thread implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_UP) {
-            if(direction != 2) direction = 0;
+            addDirection(0);
         } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            if(direction != 0) direction = 2;
+            addDirection(2);
         } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            if(direction != 1) direction = 3;
+            addDirection(3);
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            if(direction != 3) direction = 1;
+            addDirection(1);
         }
     }
 
@@ -110,5 +117,48 @@ public class Snake extends Thread implements KeyListener {
 
     }
 
+    public boolean invalidDirection(int direction, int index) {
+        return (direction == movementBuffer[index] ||
+                direction == 0 && movementBuffer[index] == 2 ||
+                direction == 1 && movementBuffer[index] == 3 ||
+                direction == 2 && movementBuffer[index] == 0 ||
+                direction == 3 && movementBuffer[index] == 1);
+    }
 
+    synchronized public void addDirection(int direction) {
+        if(bufferUtilized && bufferContains == 1) {
+
+            if(invalidDirection(direction, 0)) return;
+
+            movementBuffer[0] = direction;
+            bufferUtilized = false;
+        } else if(bufferContains == 1) {
+            System.out.println("Entered 1");
+
+            if(invalidDirection(direction, 0)) return;
+
+            System.out.println("Direction added");
+            movementBuffer[1] = direction;
+            bufferContains++;
+            bufferUtilized = false;
+        } else {
+            System.out.println("Entered 2");
+
+            if(invalidDirection(direction, 1)) return;
+
+            movementBuffer[0] = movementBuffer[1];
+            movementBuffer[1] = direction;
+
+            System.out.println("Direction replaced");
+            bufferUtilized = false;
+        }
+    }
+
+    synchronized public void removeDirection() {
+        if(bufferContains == 2) {
+            movementBuffer[0] = movementBuffer[1];
+            movementBuffer[1] = -1;
+            bufferContains--;
+        }
+    }
 }
